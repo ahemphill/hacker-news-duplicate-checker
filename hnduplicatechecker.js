e513a775a3c720e5dddf46aff2db0b9ea2b1017a
ahemphill <ahemphill@users.noreply.github.com>
2016-09-18 22:14:56 -0700

    Display results

--- hnduplicatechecker.js
+++ hnduplicatechecker.js
@@ -1,7 +1,8 @@
 let hnDuplicateChecker = (function(){
   let input = document.querySelector( 'input[name=url]' ),
     elem = document.createElement( 'a' ),
-    yearAgo = Math.floor( Date.now() / 1000 ) - 31540000,
+    secondsNow = Math.floor( Date.now() / 1000 ),
+    yearAgo = secondsNow - 31540000,
     request;
 
   input.addEventListener( 'blur', getDuplicates );
@@ -18,6 +19,36 @@ let hnDuplicateChecker = (function(){
     }
   }
 
+  function timeAgo( secondsThen, secondsNow = Math.floor( Date.now() / 1000 ), unit = 'day' ){
+    //TODO: Return the largest unit without needing a parameter
+    let secondsDiff = secondsNow - secondsThen,
+      num, phrase,
+      singular = 'a';
+    switch ( unit ){
+      case 'year':
+        num = Math.floor( secondsDiff / 31540000 );
+        break;
+      case 'month':
+        num = Math.floor( secondsDiff / 2628000 );
+        break;
+      case 'week':
+        num = Math.floor( secondsDiff / 604800 );
+        break;
+      case 'day':
+        num = Math.floor( secondsDiff / 86400 );
+        break;
+      case 'hour':
+        num = Math.floor( secondsDiff / 3600 );
+        singular = 'an';
+        break;
+      case 'minute':
+        num = Math.floor( secondsDiff / 60 );
+        break;
+    }
+    phrase = ( num <=1 ? singular : num ) + ` ${unit}` + ( num > 1 ? 's' : '');
+    return [ num, phrase ];
+  }
+
   function processDuplicates(){
     if ( request.readyState === XMLHttpRequest.DONE && request.status === 200 ){
       let prevDupes = document.getElementById( 'duplicates' ),
@@ -28,7 +59,7 @@ let hnDuplicateChecker = (function(){
       if ( prevDupes ){
         prevDupes.parentElement.removeChild( prevDupes );
       }
-      if ( response.nbHits > 0 ){
+      if ( response.nbHits > 0 && response.hits[0]._highlightResult.url.fullyHighlighted == true ){
         for ( let i = 0; i < 2; i++ ){
           td = document.createElement( 'td' );
           tr.appendChild( td );
@@ -37,20 +68,24 @@ let hnDuplicateChecker = (function(){
               td.innerText = 'dupes';
               break;
             case 1:
-              // TODO: Filter on hit._highlightResult.fullyHighlighted == true, num_comments > 1; calculate timeAgo; style
+              // TODO: Style
               ul = document.createElement( 'ul' );
-              for ( let i = 0; i < 5; i++ ){ // TODO: Add <5 failsafe
-                let li = document.createElement( 'li' ),
-                  a = document.createElement( 'a' ),
-                  hit = response.hits[i],
-                  timeAgo = 'someTime';
-                a.href = `item?id=${hit.objectID}`;
-                a.innerText = `${hit.title} (${hit.points} points by ${hit.author} ${timeAgo} ago | ${hit.num_comments} comments)`;
-                li.appendChild( a );
-                ul.appendChild( li );
+              for ( let i = 0; /*hit.num_comments > 1 &&*/ i < response.nbHits; i++ ){
+                hit = response.hits[i];
+                if ( typeof hit != 'undefined' ){
+                  let li = document.createElement( 'li' ),
+                    a = document.createElement( 'a' );
+                    a2 = document.createElement( 'a' );
+                  a.href = `item?id=${hit.objectID}`;
+                  a.style.cssText = 'color: #828282;';
+                  a2.href = `${hit.url}`;
+                  a.innerText = `${hit.title} [${a2.hostname}] ( ${hit.points} points by ${hit.author} ${timeAgo( hit.created_at_i )[1]} ago | ${hit.num_comments} comments )`;
+                  li.appendChild( a );
+                  ul.appendChild( li );
+                  ul.style.cssText = 'list-style-type: none; padding-left: 0; margin: 0;';
+                }
               }
               td.appendChild( ul );
-              // td.innerText = `${response.nbHits} similar URLs have been submitted in the past year`;
               break;
           }
         }
